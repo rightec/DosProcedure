01555ac1f9268df3968185fd52d4d15d87139bd2
Fernando Morani <morani.rightec@gmail.com>
2020-07-26 16:58:25 +0200

    Aggiunta procedura DOS per effettuare il Backup da Dir sorgente a dir destinazione

---
+++ inc_BackUp.bat
@@ -0,0 +1,77 @@
+@ECHO OFF
+
+set BATCHPATH=%cd% 
+echo %BATCHPATH% is the current directory >CON
+
+:: *** 2 command line parameters needed
+IF [%2]==[] GOTO Syntax
+
+:: ** first parameter must be a valid file specification
+rem IF NOT EXIST %1 GOTO Syntax
+:: *** first parameter must be a file name, not a directory
+rem DIR /A-D /B %~nx1 2>NUL | FIND /I "%~nx1" >NUL
+rem IF ERRORLEVEL 1 GOTO Syntax
+
+
+SET SOURCEDIR=%1
+rem C:\lavoro\Clienti\Polverari\VxDataTestDir
+SET BACKUPDIR=%2
+rem C:\lavoro\Clienti\Polverari\VxDataBackUpDir
+
+:: Move on sourced dir
+cd %SOURCEDIR%
+
+FOR %%I in (*.*) DO (
+     ::Echo checking %%I >CON
+     IF NOT EXIST %BACKUPDIR%\%%I (
+         ECHO %%I doesn't exist in %BACKUPDIR%>CON
+         ::Copying the file in BackUp
+         copy %%I  %BACKUPDIR%\%%I
+     ) ELSE call:SalvaDati %SOURCEDIR% %BACKUPDIR% %%I
+)
+
+:: Back on batch dir
+cd %BATCHPATH%
+pause
+
+GOTO:EOF
+ 
+:Syntax
+ECHO.>CON
+ECHO inc_backUp.bat,  Version 1.00 for Windows>CON
+ECHO Perform incremental backup>CON
+ECHO Written by Fernando Morani>CON
+ECHO USAGE:>CON
+ECHO inc_backUp.bat accepts 2 input paramters - That's Mandatory>CON
+GOTO:EOF
+
+
+:SalvaDati
+
+SET FIRSTFILESIZE=0
+SET SECONDFILESIZE=0
+
+:: Check specified first file's actual size
+SET temp=%1
+SET tempFile="%3"
+
+set FIRSTFILESIZE=%~z3
+::echo FIRSTFILESIZE is %FIRSTFILESIZE%
+
+:: Check specified second file's actual size
+SET temp=%2
+SET tempFile="%3"
+
+FOR /F "usebackq" %%A IN ('%2\%3') DO set SECONDFILESIZE=%%~zA
+
+::echo SECONDFILESIZE is %SECONDFILESIZE%
+
+:: Compare file's size and verify the result
+IF %FIRSTFILESIZE% NEQ %SECONDFILESIZE% (
+    rem echo First is %FIRSTFILESIZE%
+    rem echo Second is %SECONDFILESIZE%
+    echo Different sizes copying the file %tempFile%
+    copy %tempFile%  %BACKUPDIR%\%tempFile%
+)
+GOTO:EOF
+
